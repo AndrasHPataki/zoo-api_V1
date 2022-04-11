b5f757164c83a543c7f7b5a894e8f2991093d03d
AndrasHPataki <33844385+AndrasHPataki@users.noreply.github.com>
2022-04-11 05:07:52 -0300

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,40 @@
+from flask_testing import TestCase
+import unittest
+import flask_testing
+from main import app,zoo
+class MyTest(TestCase):
+
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_see_all_endpoint(self):
+        response = self.client.get('/see-all')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json,zoo['animals'])
+    
+    def test_create_animal(self):
+        data = {"name":"Duck","age":5}
+        response = self.client.post('/create-animal',json=data)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_animal(self):
+        data = {"name":"Hen","age":5}
+        response = self.client.put('/update-animal/Duck',json=data)
+        self.assertEqual(response.status_code,200)
+    
+    def test_delete_animal(self):
+        response = self.client.delete('/delete-animal/Hen')
+        self.assertEqual(response.status_code,200)
+    
+    def test_partial_animal_update(self):
+        data = {"name":"Hen","age":16}
+        response = self.client.patch('/partial-update/Hen',json=data)
+        self.assertEqual(response.status_code,200)
+    
+    def test_error_statement(self):
+        response = self.client.get('/random-url')
+        self.assertEqual(response.status_code,404)
+
+if __name__ == '__main__':
+    unittest.main()
