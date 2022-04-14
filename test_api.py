660d428ac8ccd4e4d7431798ea7d01aaec796000
AndrasHPataki <33844385+AndrasHPataki@users.noreply.github.com>
2022-04-14 13:55:23 -0300

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -8,28 +8,28 @@ class MyTest(TestCase):
         app.config['TESTING'] = True
         return app
 
-    def test_see_all_endpoint(self):
+    def test_1_see_all_endpoint(self):
         response = self.client.get('/see-all')
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.json,zoo['animals'])
     
-    def test_create_animal(self):
+    def test_2_create_animal(self):
         data = {"name":"Duck","age":5}
         response = self.client.post('/create-animal',json=data)
         self.assertEqual(response.status_code, 200)
     
-    def test_update_animal(self):
+    def test_3_update_animal(self):
         data = {"name":"Hen","age":5}
         response = self.client.put('/update-animal/Duck',json=data)
         self.assertEqual(response.status_code,200)
     
-    def test_delete_animal(self):
-        response = self.client.delete('/delete-animal/Hen')
-        self.assertEqual(response.status_code,200)
-    
-    def test_partial_animal_update(self):
+    def test_4_partial_animal_update(self):
         data = {"name":"Hen","age":16}
         response = self.client.patch('/partial-update/Hen',json=data)
+        self.assertEqual(response.status_code,200)
+        
+    def test_5_delete_animal(self):
+        response = self.client.delete('/delete-animal/Hen')
         self.assertEqual(response.status_code,200)
     
     def test_error_statement(self):
